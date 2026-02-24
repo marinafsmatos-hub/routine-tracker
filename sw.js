1e6cadcf1bce812e9a4f95789c662e58b6ad2dff
marinafsmatos-hub <marinafsmatos@gmail.com>
2026-02-24 09:47:50 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "derm-routine-cache-v1";
+const CACHE = "derm-routine-cache-v2";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
